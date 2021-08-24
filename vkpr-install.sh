a9f658bfcf0d78679e0872560214e4fc2f3f0930
Jpeedroza <pedrozajoaopedro@gmail.com>
2021-08-24 14:46:22 -0300

    Install script of rit and vkpr

--- vkpr-install.sh
+++ vkpr-install.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 echo "VKPR install script"
 echo "==================="
+curl -fsSL https://commons-repo.ritchiecli.io/install.sh | bash
+rit add repo --provider="Github" --name="vkpr-cli" --repoUrl="https://github.com/vertigobr/vkpr-cli"
+rit vkpr init
