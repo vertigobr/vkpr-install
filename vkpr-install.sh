15bc56051650b22368a5466785c8eaaea098a14e
João Pedro Pedroza <67215104+jpeedroza@users.noreply.github.com>
2021-08-25 09:32:43 -0300

    Changed the formula-runner of rit

--- vkpr-install.sh
+++ vkpr-install.sh
@@ -3,4 +3,5 @@ echo "VKPR install script"
 echo "==================="
 curl -fsSL https://commons-repo.ritchiecli.io/install.sh | bash
 rit add repo --provider="Github" --name="vkpr-cli" --repoUrl="https://github.com/vertigobr/vkpr-cli"
+rit set formula-runner --runner="local"
 rit vkpr init
