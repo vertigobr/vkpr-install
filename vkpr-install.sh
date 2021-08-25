070f02931df8e4469ec307229da4fcaccc172e75
João Pedro Pedroza <67215104+jpeedroza@users.noreply.github.com>
2021-08-25 12:04:22 -0300

    Update on rit download and information creation

--- vkpr-install.sh
+++ vkpr-install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-echo "VKPR install script"
-echo "==================="
-curl -fsSL https://commons-repo.ritchiecli.io/install.sh | bash
+echo "$(printf '\033[1m')$(printf '\033[32m')VKPR install script$(printf '\033[0m')"
+echo "$(printf '\033[1m')===================$(printf '\033[0m')"
+if [ $(id -u) -eq 0 ]; then
+  curl -fsSL https://commons-repo.ritchiecli.io/install.sh | sed -e 's/sudo//g' | bash
+else
+  curl -fsSL https://commons-repo.ritchiecli.io/install.sh | bash
+fi
 rit add repo --provider="Github" --name="vkpr-cli" --repoUrl="https://github.com/vertigobr/vkpr-cli"
 rit set formula-runner --runner="local"
 rit vkpr init
+echo "\n$(printf '\033[4m')$(printf '\033[1m')$(printf '\033[31m')Caution!$(printf '\033[0m')\n"
+echo "  To use VKPR, you must have Docker installed."
+echo "  It is recommended to use aliases to make writing VKPR commands easier, like:\n"
+echo "  $(printf '\033[32m')alias$(printf '\033[0m') vkpr=\"rit vkpr\"\n"
